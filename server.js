b09f70f9a8220627cbc79ea44e81bda570876e8c
Nikolay Stoitsev <nstoitsev@gmail.com>
2013-05-13 13:37:38 +0300

    Added auto determing of ip address

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
 var express = require('express');
-
+var async = require('async');
 
 var app = express.createServer();
 
 var io = require('socket.io').listen(app);
 
-io.sockets.on('connection', function (socket) {
-    console.log('connected')
-});
+var serverIP = '';
 
-app.configure(function(){
-  app.use('/media', express.static(__dirname + '/media'));
-});
+async.series([
+    function getIPAddres (cb) {
+        var interfaces = require('os').networkInterfaces();
+        for (var devName in interfaces) {
+            var iface = interfaces[devName];
 
-app.get('/', function(req, res){
-    res.render('index.jade', {layout: false});
-});
+            for (var i = 0; i < iface.length; i++) {
+                var alias = iface[i];
+                if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && alias.address !== '127.0.1.1' && !alias.internal)
+                    serverIP = alias.address;
+            }
+        }
 
-io.sockets.on('connection', function (socket) {
-    socket.on('command', function (msg) { 
-        console.log(msg);
-    });
-});
+        cb();
+    },
+    function startServer (cb) {
+        app.configure(function(){
+          app.use('/media', express.static(__dirname + '/media'));
+        });
 
-app.listen(8000);
+        app.get('/', function(req, res){
+            res.render('index.jade', {layout: false});
+        });
+
+        io.sockets.on('connection', function (socket) {
+            socket.on('command', function (msg) {
+                console.log(msg);
+            });
+        });
+
+        app.listen(8000);
+        console.log('started server', serverIP);
+    }
+]);
+
